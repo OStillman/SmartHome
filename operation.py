2e90291367d370e49ea97fad271128a35b67da72
= <owenstillman@live.co.uk>
2020-05-16 14:37:22 +0100

    Fix double open/close

--- operation.py
+++ operation.py
@@ -14,20 +14,25 @@ class DoorWatcher:
         self.runWatcher()
 
     def runWatcher(self):
+        doorOpen = False
         while self.procExe.poll() is None:
             line = self.procExe.stdout.readline()
             #print("Print:" + line)
             if constants.door_sensor_open in line:
-                print("Door Open")
-                on_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("group", constants.hallway, True, constants.hallway_brightness))
-                off_motion_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, False))
-                on_thread.start()
-                off_motion_thread.start()
+                if not doorOpen:
+                    print("Door Open")
+                    doorOpen = True
+                    on_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("group", constants.hallway, True, constants.hallway_brightness))
+                    off_motion_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, False))
+                    on_thread.start()
+                    off_motion_thread.start()
                 #lights.SimpleLightsToggle("group", constants.hallway_brightness, constants.hallway, True)
             elif constants.door_sensor_close in line:
-                print("Door Closed")
-                off_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, True))
-                off_thread.start()
+                if doorOpen:
+                    print("Door Closed")
+                    doorOpen = False
+                    off_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, True))
+                    off_thread.start()
             elif line != "":
                 print(line)
 
