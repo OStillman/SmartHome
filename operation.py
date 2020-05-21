4742c895c2bac69d4b635012180ca50fc0ed944f
= <owenstillman@live.co.uk>
2020-05-21 16:13:41 +0100

    Create Devmode Stub

--- operation.py
+++ operation.py
@@ -10,31 +10,43 @@ from Hue import light_control as lights
 
 class DoorWatcher:
     def __init__(self):
-        self.procExe = subprocess.Popen("./receiver.sh", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        self.runWatcher()
+        self.doorOpen = False
+        if constants.__DevMode__:
+            self.DevMode()
+        else:
+            self.procExe = subprocess.Popen("./receiver.sh", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+            self.runWatcher()
 
     def runWatcher(self):
-        doorOpen = False
         while self.procExe.poll() is None:
             line = self.procExe.stdout.readline()
             #print("Print:" + line)
             if constants.door_sensor_open in line:
-                if not doorOpen:
-                    print("Door Open")
-                    doorOpen = True
-                    on_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("group", constants.hallway, True, constants.hallway_brightness))
-                    off_motion_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, False))
-                    on_thread.start()
-                    off_motion_thread.start()
+                if not self.doorOpen:
+                    self.DoorOpened()
                 #lights.SimpleLightsToggle("group", constants.hallway_brightness, constants.hallway, True)
             elif constants.door_sensor_close in line:
-                if doorOpen:
-                    print("Door Closed")
-                    doorOpen = False
-                    off_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, True))
-                    off_thread.start()
+                if self.doorOpen:
+                    self.DoorClosed()                    
             elif line != "":
                 print(line)
+
+    def DevMode(self):
+        print("Devmode on")
+
+    def DoorOpened(self):
+        print("Door Open")
+        self.doorOpen = True
+        on_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("group", constants.hallway, True, constants.hallway_brightness))
+        off_motion_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, False))
+        on_thread.start()
+        off_motion_thread.start()
+
+    def DoorClosed(self):
+        print("Door Closed")
+        self.doorOpen = False
+        off_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, True))
+        off_thread.start()
 
 
 DoorWatcher()
