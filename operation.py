0e35b6abc5248ae8f36fe27f492d3fdbb311b3c8
= <owenstillman@live.co.uk>
2020-05-21 17:08:35 +0100

    Offload SM logic to Pi4

--- operation.py
+++ operation.py
@@ -4,6 +4,9 @@ import os, sys
 import subprocess
 import constants
 import threading
+import time
+import requests
+import json
 
 from Hue import light_control as lights
 
@@ -33,20 +36,42 @@ class DoorWatcher:
 
     def DevMode(self):
         print("Devmode on")
+        time.sleep(2)
+        self.DoorOpened()
+        time.sleep(2)
+        self.DoorClosed()
+
 
     def DoorOpened(self):
         print("Door Open")
+        self.doorOpen = True
+        payload = {"Sensor": "Front_Door", "Status": "Open"}
+        open_thread = threading.Thread(target=self.sendRequest, args=(payload,))
+        open_thread.start()
+        '''
         self.doorOpen = True
         on_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("group", constants.hallway, True, constants.hallway_brightness))
         off_motion_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, False))
         on_thread.start()
         off_motion_thread.start()
+        '''
 
     def DoorClosed(self):
         print("Door Closed")
         self.doorOpen = False
+        payload = {"Sensor": "Front_Door", "Status": "Closed"}
+        close_thread = threading.Thread(target=self.sendRequest, args=(payload,))
+        close_thread.start()
+        '''
+        self.doorOpen = False
         off_thread = threading.Thread(target=lights.SimpleLightsToggle, args=("motion_sensor", constants.motion_sensor_id, True))
         off_thread.start()
+        '''
 
+    def sendRequest(self, payload):
+        if constants.__DevMode__:
+            requests.post("http://raspberrypi.local:5000/door", data=json.dumps(payload))
+        else:
+            requests.post("http://raspberrypi.local/door", data=json.dumps(payload))
 
 DoorWatcher()
